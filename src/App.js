8e5443ca8efa64111c2e4fbb260e07dbb9e98c44
Alejo Ufano <ufanoalejo@gmail.com>
2022-09-30 12:56:36 -0300

    Fix(App) App changes/update

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ import {API_KEY} from './utils/weather'
 
 function App() {
   let [cities, setCities] = useState([])
-  function onClose(id) {
-    setCities(oldCities => oldCities.filter(c => c.id !== id));
-  }
   let getCity = async (city) => {
-    console.log('city:', city);
     try {
       let data = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
       if(cities.filter(c=>c.id===data.data.id).length){
         return alert(`${data.data.name} city is already loaded`)
       } else {
-        setCities(oldCities => [...oldCities, data.data])
+        setCities([data.data])
       } 
     } catch (e) {
       return alert(`${city} not found!`)
@@ -26,9 +22,9 @@ function App() {
 
   },[setCities])
   return (
-    <div className="globalContainer">
-      <NavBar getCity={getCity}/>
-      <Home cities={cities} onClose={onClose}/>
+    <div className='globalContainer'>
+      <NavBar getCity={getCity} />
+      <Home cities={cities} />
     </div>
   );
 }
